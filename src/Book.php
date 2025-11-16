25b5a6a1ce2af99a4befef833b12aafa8fa100bf
Iliya <git919nova@gmail.com>
2025-11-16 11:24:22 +0330

    Update Book.php

--- src/Book.php
+++ src/Book.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types=0);
 
 require_once __DIR__ . '/Product.php';
 require_once __DIR__ . '/Sellable.php';
@@ -9,25 +9,29 @@ require_once __DIR__ . '/Sellable.php';
  */
 class Book extends Product implements Sellable
 {
-    /**
-     * TODO(Task 3):
-     * Implement a constructor with the signature:
-     *
-     * public function __construct(int $id, string $title, int $price)
-     *
-     * It must assign the arguments to the protected properties inherited
-     * from Product. You do NOT have to use parent::__construct(); you can
-     * directly assign to $this->id, $this->title, $this->price.
-     */
+    public function __construct(int $id, string $title, int $price)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+    }
 
-    /**
-     * TODO(Task 3):
-     * Implement getTypeLabel() so that it returns the exact Persian string "کتاب".
-     */
 
-    /**
-     * TODO(Task 3):
-     * Implement getFinalPrice() so that it returns the price with 10% discount.
-     * Example: price = 100000 → final price = 90000.
-     */
+
+    public function getTypeLabel(): string
+    {
+        return "کتاب";
+    }
+  
+
+
+    public function getFinalPrice(): int
+    {
+        return (int) round($this->price * 0.9);
+    }
 }
+
+
+
+
+
