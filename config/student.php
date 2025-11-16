a8e2c378d98beb4a26acd6d679067fc45951f057
Iliya <git919nova@gmail.com>
2025-11-16 11:12:24 +0330

    Update student.php

--- config/student.php
+++ config/student.php
@@ -7,6 +7,6 @@ declare(strict_types=1);
  * هر دانشجو باید این مقادیر را با اطلاعات خودش کامل کند.
  */
 
-const STUDENT_ID = 'TODO_STUDENT_ID'; // مثال: "40123456"
-const STUDENT_NAME = 'TODO_FULL_NAME'; // مثال: "Sara Ahmadi"
-const STUDENT_SECRET_PHRASE = 'TODO_ONE_SHORT_UNIQUE_SENTENCE_IN_PERSIAN';
+const STUDENT_ID = '17'; // مثال: "40123456"
+const STUDENT_NAME = 'ilya mirkhosravi'; // مثال: "Sara Ahmadi"
+const STUDENT_SECRET_PHRASE = 'lannister always pays his debts';
