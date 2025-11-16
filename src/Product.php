b9d900b510eae65c7ce33c876a76f2443db9fe26
Iliya <git919nova@gmail.com>
2025-11-16 11:26:15 +0330

    Update Product.php

--- src/Product.php
+++ src/Product.php
@@ -1,49 +1,34 @@
 <?php
-declare(strict_types=1);
+declare(strict_types=0);
 
-/**
- * Abstract base class for all products in this exam.
- *
- * NOTE TO STUDENTS:
- * - Do NOT rename this class or its methods.
- * - Complete all TODO sections as described in README.md.
- */
+
 abstract class Product
 {
-    // TODO(Task 1):
-    // Add protected properties:
-    // - int $id
-    // - string $title
-    // - int $price
+     
+   protected int $id;
+   protected string $title;
+   protected int $price;
+  
 
-    /**
-     * TODO(Task 1):
-     * Implement a constructor with the exact signature:
-     *
-     * public function __construct(int $id, string $title, int $price)
-     *
-     * It must assign the arguments to the protected properties.
-     */
+     public function __construct(int $id, string $title, int $price)
+{
+    $this->id = $id;
+    $this->title = $title;
+    $this->price = $price;
+}
 
-    /**
-     * TODO(Task 1):
-     * Implement this method to return the product title.
-     *
-     * public function getTitle(): string
-     */
 
-    /**
-     * TODO(Task 1):
-     * Implement this method to return the product price.
-     *
-     * public function getPrice(): int
-     */
+    public function getTitle(): string
+{
+    return $this->title;
+}
 
-    /**
-     * Each subclass must return a human-readable type label,
-     * for example "Book" or "Notebook".
-     *
-     * This method MUST stay abstract.
-     */
+public function getPrice(): int
+{
+    return $this->price;
+}
+
+   
     abstract public function getTypeLabel(): string;
 }
+
